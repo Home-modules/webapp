9160526b5555cf3f234e9db0509a1ae90a74241c
Mammad900 <mohammadhosseinabedi052@gmail.com>
2022-06-12 18:14:05 +0430

    Improve icon selector accessibility & keyboard use

--- src/ui/fields/icon-select.tsx
+++ src/ui/fields/icon-select.tsx
@@ -1,7 +1,6 @@
 import { IconDefinition, IconName } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import { HMApi } from "../../hub/api";
 import { uniqueId } from "../../uniqueId";
 import './icon-select.scss';
 
@@ -21,15 +20,17 @@ export default function IconSelect({icons, value, onChange}: IconSelectProps) {
     const valueIndex = icons.findIndex(i=> i.iconName === value);
 
     React.useEffect(()=> {
-        const inAnimation = !squareCoordinates; // When the component mount, it is inside a scale animation from 75% to 100% and squareCoordinates isn't yet set.
+        // const inAnimation = !squareCoordinates; // When the component mount, it is inside a scale animation from 75% to 100% and squareCoordinates isn't yet set.
         if(ref.current) {
-            const child = ref.current.childNodes[valueIndex] as SVGSVGElement
-            const parentCoordinates = ref.current.getBoundingClientRect();
-            const coordinates = child.getBoundingClientRect();
-            console.log(parentCoordinates, coordinates);
+            const child = ref.current.childNodes[valueIndex] as HTMLLabelElement;
+            // const parentCoordinates = ref.current.getBoundingClientRect();
+            // const coordinates = child.getBoundingClientRect();
+            // console.log(parentCoordinates, coordinates);
             setSquareCoordinates([
-                (coordinates.x - parentCoordinates.x)*(inAnimation?(1/0.75) : 1), 
-                (coordinates.y - parentCoordinates.y)*(inAnimation?(1/0.75) : 1)
+                // (coordinates.x - parentCoordinates.x)*(inAnimation?(1/0.75) : 1), 
+                // (coordinates.y - parentCoordinates.y)*(inAnimation?(1/0.75) : 1)
+                child.offsetLeft,
+                child.offsetTop
             ])
         }
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -38,11 +39,20 @@ export default function IconSelect({icons, value, onChange}: IconSelectProps) {
     return(
         <div className='icon-select' data-icon={value} ref={ref}>
             {icons.map((icon, index) => (
-                <FontAwesomeIcon key={icon.iconName} icon={icon} onClick={()=> {
-                    if(value !== icon.iconName) {
-                        onChange(icon.iconName);
-                    }
-                }} className={icon.iconName===value? 'selected':''} />
+                <label key={icon.iconName}>
+                    <input 
+                        type='radio' 
+                        name={id} 
+                        value={icon.iconName} 
+                        checked={icon.iconName===value} 
+                        onChange={(e)=> {
+                            if(e.target.checked) {
+                                onChange(icon.iconName);
+                            }
+                        }}
+                    />
+                    <FontAwesomeIcon icon={icon} />
+                </label>
             ))}
             <div className="square" style={squareCoordinates && {left: squareCoordinates[0]+'px', top: squareCoordinates[1]+'px'}} />
         </div>
