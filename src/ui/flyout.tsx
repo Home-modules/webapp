40da858ef913d508566f77f76de13a7dac9e967a
Mammad <mohammadhosseinabedi052@gmail.com>
2023-03-17 00:28:15 +0330

    Format flyout.tsx, allow navigating via keyboard

--- src/ui/flyout.tsx
+++ src/ui/flyout.tsx
@@ -9,7 +9,7 @@ import { connect } from 'react-redux';
 export type FlyoutProps = {
     id: string,
     element: Element,
-    children: React.ReactChild|React.ReactChild[],
+    children: React.ReactChild | React.ReactChild[],
     showCloseButton?: boolean,
     width: number,
     buttons?: ({
@@ -25,10 +25,10 @@ export type FlyoutProps = {
         async: true,
         onClick: (e: React.MouseEvent<HTMLButtonElement>) => Promise<any>,
     }))[],
-    onClose?: ()=> void,
+    onClose?: () => void,
 }
 
-export default function Flyout({id, element, children, showCloseButton, width, buttons, onClose}: FlyoutProps) {
+export default function Flyout({ id, element, children, showCloseButton, width, buttons, onClose }: FlyoutProps) {
     const containerRef = React.useRef(null);
     const [position, setPosition] = React.useState(getFlyoutPosition(element, width));
     const [closing, setClosing] = React.useState(false);
@@ -36,7 +36,7 @@ export default function Flyout({id, element, children, showCloseButton, width, b
     function handleClose() {
         onClose?.();
         setClosing(true);
-        setTimeout(()=> {
+        setTimeout(() => {
             store.dispatch({
                 type: "REMOVE_FLYOUT",
                 id
@@ -44,29 +44,39 @@ export default function Flyout({id, element, children, showCloseButton, width, b
         }, 500);
     }
 
-    React.useEffect(()=> {
-        const observer = new ResizeObserver(()=> {
+    React.useEffect(() => {
+        const observer = new ResizeObserver(() => {
             setPosition(getFlyoutPosition(element, width));
         });
         observer.observe(element);
-    })
+    });
+
+    function onKeyDown(e: React.KeyboardEvent<HTMLButtonElement>) {
+        const element =
+            e.key === 'ArrowLeft' ?
+                (e.target as HTMLElement).previousElementSibling :
+            e.key === 'ArrowRight' ?
+                (e.target as HTMLElement).nextElementSibling :
+            null;
+        element && (element as HTMLElement).focus();
+    }
 
     return (
-        <div ref={containerRef} className={`flyout-container ${closing?'closing':''}`} onClick={e=> {
-            if(e.target === containerRef?.current) {
+        <div ref={containerRef} className={`flyout-container ${closing ? 'closing' : ''}`} onClick={e => {
+            if (e.target === containerRef?.current) {
                 handleClose();
             }
         }}>
-            <div className={`flyout ${position.top!==undefined ? 'bottom':'top'}`} style={{...position, width}}>
+            <div className={`flyout ${position.top !== undefined ? 'bottom' : 'top'}`} style={{ ...position, width }}>
                 {showCloseButton && (
                     <FontAwesomeIcon icon={faTimes} className='close' onClick={handleClose} />
                 )}
                 <div className="content">
                     {children}
                 </div>
-                {buttons && 
+                {buttons &&
                     <div className="buttons">
-                        {buttons.map((button, index)=> (
+                        {buttons.map((button, index) => (
                             button.async ? (
                                 <IntermittentButton
                                     key={index}
@@ -74,25 +84,29 @@ export default function Flyout({id, element, children, showCloseButton, width, b
                                     primary={button.primary}
                                     attention={button.attention}
                                     disabled={button.disabled}
-                                    onClick={(e)=> button.onClick(e).then((res)=> {
-                                        if(button.closeOnClick!==false) {
+                                    onClick={(e) => button.onClick(e).then((res) => {
+                                        if (button.closeOnClick !== false) {
                                             handleClose();
                                         }
                                     })}
+                                    autoFocus={index === 0}
+                                    onKeyDown={onKeyDown}
                                 />
                             ) : (
-                                <Button 
+                                <Button
                                     key={index}
                                     children={button.text}
                                     primary={button.primary}
                                     attention={button.attention}
                                     disabled={button.disabled}
-                                    onClick={e=> {
+                                    onClick={e => {
                                         button.onClick?.(e);
-                                        if(button.closeOnClick!==false) {
+                                        if (button.closeOnClick !== false) {
                                             handleClose()
                                         }
                                     }}
+                                    autoFocus={index === 0}
+                                    onKeyDown={onKeyDown}
                                 />
                             )
                         ))}
@@ -103,39 +117,39 @@ export default function Flyout({id, element, children, showCloseButton, width, b
     )
 }
 
-export const Flyouts = connect(({flyouts}: StoreState)=>({flyouts}))(
-    function Flyouts({flyouts}: Pick<StoreState, 'flyouts'>) {
-        return <>{flyouts.map(flyout => <Flyout key={flyout.id} {...flyout}/>)}</>
+export const Flyouts = connect(({ flyouts }: StoreState) => ({ flyouts }))(
+    function Flyouts({ flyouts }: Pick<StoreState, 'flyouts'>) {
+        return <>{flyouts.map(flyout => <Flyout key={flyout.id} {...flyout} />)}</>
     }
 )
 
 function getFlyoutPosition(element: Element, width: number) {
     const rect = element.getBoundingClientRect();
-    let left: number = rect.x + rect.width/2 - width/2;
-    let bottom: number|undefined = window.innerHeight - rect.y + 10;
-    let top: number|undefined = undefined;
+    let left: number = rect.x + rect.width / 2 - width / 2;
+    let bottom: number | undefined = window.innerHeight - rect.y + 10;
+    let top: number | undefined = undefined;
 
-    if(window.innerHeight - bottom < 150) {
+    if (window.innerHeight - bottom < 150) {
         bottom = undefined;
         top = rect.bottom + 10;
     }
 
-    if(left < 10) {
+    if (left < 10) {
         left = 10;
     }
 
-    if(left + width > window.innerWidth - 10) {
+    if (left + width > window.innerWidth - 10) {
         left = window.innerWidth - 10 - width;
     }
 
-    return {left, bottom, top}
+    return { left, bottom, top }
 }
 
-export function addConfirmationFlyout({element, text, confirmText, attention = false, onConfirm, async, width = 200}: {
-    element: EventTarget|Element,
-    text: string|JSX.Element, 
-    confirmText: string, 
-    attention?: boolean, 
+export function addConfirmationFlyout({ element, text, confirmText, attention = false, onConfirm, async, width = 200 }: {
+    element: EventTarget | Element,
+    text: string | JSX.Element,
+    confirmText: string,
+    attention?: boolean,
     width?: number,
 } & ({
     async?: false,
