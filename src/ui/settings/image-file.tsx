e8f6957ce3cab1cedf7e19978243d0fc2df9be2e
Mammad <mohammadhosseinabedi052@gmail.com>
2023-03-17 00:26:41 +0330

    Allow image fields to be focused by keyboard

--- src/ui/settings/image-file.tsx
+++ src/ui/settings/image-file.tsx
@@ -3,39 +3,43 @@ import SettingItem, { SettingItemProps } from "./setting";
 import "./image-file.scss"
 
 export type SettingItemImageFileProps = SettingItemProps & {
-    value: string|null,
+    value: string | null,
     onChange: (value: string | null) => void
 }
 
-export function SettingItemImageFile({ title, description, icon, className='', value, onChange }: SettingItemImageFileProps) {
+export function SettingItemImageFile({ title, description, icon, className = '', value, onChange }: SettingItemImageFileProps) {
     return (
         <SettingItem icon={icon} title={title} description={description} className={`image-file ${className}`}>
-            <label className="value">
+            <label className="value" tabIndex={0}>
                 {value && <img src={value} alt={title} />}
-                <div className={`label ${value ? 'backdrop':''}`}>Select file</div>
-                <input type="file" accept="image/*" onChange={e => {
-                    const file = (e.target as HTMLInputElement).files?.[0];
-                    if (file) {
-                        const fr = new FileReader();
-                        fr.onload = e => {
-                            if (e.target?.result) {
-                                onChange(e.target.result as string);
-                            } else {
-                                store.dispatch({
-                                    type: "ADD_NOTIFICATION",
-                                    notification: {
-                                        type: "error",
-                                        message: "Failed to read the file"
-                                    }
-                                });
-                                onChange(null);
+                <div className={`label ${value ? 'backdrop' : ''}`}>Select file</div>
+                <input
+                    type="file"
+                    accept="image/*"
+                    onChange={e => {
+                        const file = (e.target as HTMLInputElement).files?.[0];
+                        if (file) {
+                            const fr = new FileReader();
+                            fr.onload = e => {
+                                if (e.target?.result) {
+                                    onChange(e.target.result as string);
+                                } else {
+                                    store.dispatch({
+                                        type: "ADD_NOTIFICATION",
+                                        notification: {
+                                            type: "error",
+                                            message: "Failed to read the file"
+                                        }
+                                    });
+                                    onChange(null);
+                                }
                             }
+                            fr.readAsDataURL(file);
+                        } else {
+                            onChange(null);
                         }
-                        fr.readAsDataURL(file);
-                    } else {
-                        onChange(null);
-                    }
-                }} />
+                    }}
+                />
             </label>
         </SettingItem>
     )
