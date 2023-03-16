8de78f66fe8922ed9bab51023330640a6a77f391
Mammad <mohammadhosseinabedi052@gmail.com>
2023-03-17 00:24:20 +0330

    Unselect appearance setting when clicking empty area

--- src/screens/settings/appearance/appearance.tsx
+++ src/screens/settings/appearance/appearance.tsx
@@ -32,7 +32,7 @@ export default function SettingsPageAppearance() {
                     />
                 </section>
 
-                <div className="rest"/>
+                <div className="rest" onClick={()=>setSearchParams({})}/>
             </div>
 
             <div className={`preview ${currentField || 'closed'}`}>
